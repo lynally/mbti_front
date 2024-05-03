a50c9542584cc48c6ae874c913f30e546d3d7323
liuyn <2212770755@qq.com>
2024-05-03 15:19:40 +0800

    update: 测试打包

--- src/router/modules/home.ts
+++ src/router/modules/home.ts
@@ -34,14 +34,6 @@ export default {
       meta: {
         title: "结果"
       }
-    },
-    {
-      path: "/aboutHome",
-      name: "about",
-      component: () => import("@/views/about.vue"),
-      meta: {
-        title: "关于"
-      }
     }
   ]
 } as RouteConfigsTable;
